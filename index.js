13dff59777ca1889149b98f18b872d3ae252f5f1
ayran louro <ayranlouro@hotmail.com>
2021-04-19 18:54:48 -0300

    Second:crud

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 const ejs = require('ejs');
+const bodyParser = require('body-parser');
+const port = 80;
+
+app.use(bodyParser.json());
 
 app.set('view engine', 'ejs');
 
@@ -8,6 +12,15 @@ app.get('/', (req, res) => {
     res.render('pages/welcome');
 });
 
-app.listen(80, () => {
+app.get('/list', (req, res) => {
+    res.render('pages/list');
+});
+
+app.post('/list', (req, res) => {
+    console.log(req.body)
+    res.render('pages/list');
+});
+
+app.listen(port, () => {
     console.log('Service running on port 80...');
 });
